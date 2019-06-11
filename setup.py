054bb805b31444f1b9c5bec5ad5a6fd55df40fa0
Daniel Cerezo <danielcerezo.dev@gmail.com>
2019-06-12 11:26:03 +1200

    Created setup.py script

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="NZ_IRD_Number_Validator",
+    version="0.0.1",
+    author="Daniel Cerezo Rodriguez",
+    author_email="danielcerezo.dev@gmail.com",
+    description="A validator python package for New Zealand tax numbers (IRD)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/danielcerezodev/NZ_IRD_Number_Validator.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU License",
+        "Operating System :: OS Independent",
+    ],
+)
