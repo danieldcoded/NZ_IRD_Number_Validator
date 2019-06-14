e1e3969b817736540b6fdf416145a7fa0a9d1488
Daniel Cerezo <danielcerezo.dev@gmail.com>
2019-06-14 15:35:36 +1200

    Update package script

--- NZ_IRD_Number_Validator/__init__.py
+++ NZ_IRD_Number_Validator/__init__.py
@@ -1,35 +1,117 @@
 import re
 
 
-# This method will take the passed IRD
-# and remove all non-numeric characters
+# This method will take the passed IRD string
+# remove all non-numeric characters and add a
+# preceding 0 if the number has 8 characters
 def clean_ird(ird):
-
-    # Uses regex to remove all non-numeric characters
+    # Use regex to remove all non-numeric characters
     clean_ird_number = re.sub('[^0-9]', '', ird)
 
-    # Returns the "clean" version of the IRD
+    # add preceding 0 if IRD has 8 digits
+    if len(clean_ird_number) < 9:
+        clean_ird_number = "0" + clean_ird_number
+
+    # Returns the "clean" IRD number
     return clean_ird_number
 
 
-def __check_digit(ird, weight_factor):
-
+# This method will calculate the check digit (trailing digit of the IRD number)
+# It will multiply each digit of the IRD number to the corresponding
+# weight factor, then sums them up and gets the remainder when divided by 11.
+# The remainder is then compared to the check digit.
+def get_remainder(ird, weight_factor):
     # For checking the IRD we will need
-    PRIMARY_WEIGHTS = [3, 2, 7, 6, 5, 4, 3, 2]
-    SECONDARY_WEIGHTS = [7, 4, 3, 2, 5, 2, 7, 6]
+    primary_weights = [3, 2, 7, 6, 5, 4, 3, 2]
+    secondary_weights = [7, 4, 3, 2, 5, 2, 7, 6]
+
+    # Choose weights depending on the weight factor
+    if weight_factor == 1:
+        weights = primary_weights
+    else:
+        weights = secondary_weights
+
+    # Convert the IRD number into a list of integers
+    ird_list = list(map(int, ird[:-1]))
+
+    print(ird_list)
+    print(weights)
+
+    # Multiply each weight factor to their corresponding IRD digit
+    result = map(lambda x, y: x * y, ird_list, weights)
+
+    # Sum the result and divide it by 11 to get the remainder
+    remainder = sum(result) % 11
+
+    return int(remainder)
 
 
-# This method will take the passed IRD
-# and use the official validation process
-# to determine whether the IRD is valid or invalid
-def check_ird(ird):
+# This method will take the passed IRD number
+# and use the official validation process to
+# determine whether the IRD is valid or invalid
+def is_valid(ird):
+    # For "safety" reasons use the clean method to make sure
+    # the IRD number only contains numerical characters before
+    # validating
+    ird = clean_ird(ird)
 
-    # Initialise the boolean variable that stores
-    # whether the IRD tax number is valid or invalid
-    is_ird_valid = False
+    # # Check that the ird is the correct length
+    # # and if it is in range
+    # if len(ird[:-1]) == 8 and 10000000 <= int(ird) <= 150000000:
+    #     check_digit = ird[8]
+    #     remainder = get_remainder(ird, 1)
+    #
+    #     # If remainder is 0 and the check digit (last digit of the IRD number) is 0
+    #     # then IRD number is valid
+    #     if remainder == 0 and check_digit == remainder:
+    #         return True
+    #     # If remainder is != 0, subtract it from 11
+    #     else:
+    #         remainder = 11 - remainder
+    #     # If 11 - remainder = check digit, then IRD number is valid
+    #     if 0 <= remainder <= 9 and remainder == check_digit:
+    #         return True
+    #
+    #     # if result is 10, recalculate weights using the secondary weight factor
+    #     elif remainder == 10:
+    #         # calc for each digit using the secondary weight factor
+    #         remainder = get_remainder(ird, 2)
+    #         # if remainder is 0 and check digit (last digit of the IRD number) is 0
+    #         # then IRD number is valid
+    #         if remainder == 0 and check_digit == 0:
+    #             return True
+    #         # if remainder is != 0, subtract it from 11
+    #         else:
+    #             remainder = 11 - int(remainder)
+    #             if remainder == check_digit:
+    #                 return True
 
-    # For "safety" reasons use the clean method to make sure the IRD number
-    # only contains numerical characters before validating
-    ird_to_validate = clean_ird(ird)
+    if len((str(ird)[:-1])) == 8:  # check to see if the IRD number has the correct length
+        if 10000000 <= int(ird) <= 150000000:  # check to see if the IRD number is in the correct range
+            check_digit = ird[8]
+            remainder = get_remainder(ird, 1)  # calc to get remainder using the first weight factor
+            # if remainder is 0 and check digit (last digit of the IRD number) is 0, then IRD number is valid
+            if int(remainder) == 0 and int(check_digit) == int(remainder):
+                return True
+            # if remainder is != 0, subtract it from 11
+            elif int(remainder) != 0:
+                remainder = 11 - int(remainder)
+                #  if result is between 0 and 9
+            if 0 <= int(remainder) <= 9 and int(remainder) == int(check_digit):
+                # if remainder = check digit, then IRD number is valid
+                return True
+            # if remainder is = 10, recalculate weights using the secondary weight factor
+            elif int(remainder) == 10:
+                # calc to get remainder using the secondary weight factor
+                remainder = get_remainder(ird, 2)
+                # if remainder is 0 and check digit (last digit of the IRD number) is 0, then IRD number is valid
+                if int(remainder) == 0 and int(check_digit) == 0:
+                    return True
+                # if remainder is != 0, subtract it from 11
+                elif int(remainder) != 0:
+                    remainder = 11 - int(remainder)
+                    # if remainder = check digit, then IRD number is valid
+                    if int(remainder) == int(check_digit):
+                        return True
 
-    return is_ird_valid
+    return False
